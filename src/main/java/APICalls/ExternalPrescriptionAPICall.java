63ea44795ab2c037396948345a03a1255881ceb9
AnzaKibriya <33507051+AnzaKibriya@users.noreply.github.com>
2024-05-21 15:40:53 +0400

    minor change

--- src/main/java/APICalls/ExternalPrescriptionAPICall.java
+++ src/main/java/APICalls/ExternalPrescriptionAPICall.java
@@ -2,7 +2,6 @@ package APICalls;
 
 import com.google.gson.Gson;
 import model.ExternalPrescription;
-import model.PrescriptionRequest;
 import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -20,7 +19,7 @@ import static Helper.BaseClass.client;
 public class ExternalPrescriptionAPICall {
     static String apiUrl = "https://dawak-apim-uat.azure-api.net/dawak-patient/api/external-prescription/v2/upload";
 
-    public static String makeExternalPrescriptionAPICall(String cardFront, String cardBack, String prescriptionFile){
+    public static String makeExternalPrescriptionAPICall(String cardFront, String cardBack, String prescriptionFile) {
         MediaType mediaType = MediaType.parse("application/json");
         Gson gson = new Gson();
         ExternalPrescriptionAPICall externalPrescriptionAPICall = new ExternalPrescriptionAPICall();
@@ -48,6 +47,7 @@ public class ExternalPrescriptionAPICall {
         }
 
     }
+
     public ExternalPrescription getExternalPrescription(String cardFront, String cardBack, String prescriptionFile) {
         try (Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("/ExternalPrescription.json"))) {
             Gson gson = new Gson();
